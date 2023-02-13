16d34995c939929316c6533399cf739d2415573f
rgomezr <ramon@ramongomez.xyz>
2023-02-13 16:22:48 +0100

    Changed input file name to take ibans from

--- checkIBANs.js
+++ checkIBANs.js
@@ -21,7 +21,7 @@ var requestOptions = {
 }
 
 const readLineProcess = readLine.createInterface({
-    input: fs.createReadStream('ibansToCheck.txt'),
+    input: fs.createReadStream('placeYourIbans.txt'),
     crlfDelay: Infinity
 });
 
